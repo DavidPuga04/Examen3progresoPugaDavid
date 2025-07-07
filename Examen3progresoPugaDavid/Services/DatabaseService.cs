76d58951951d28895cab6755f613e1ff7f1d0e3e
Laboratorio <laboratorio@udla-ec.int>
2025-07-07 07:23:16 -0500

    Se creo el servicio para conectar con sqlite

---
+++ Examen3progresoPugaDavid/Services/DatabaseService.cs
@@ -0,0 +1,36 @@
+﻿using Examen3progresoPugaDavid.Models;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen3progresoPugaDavid.Services
+{
+    class DatabaseService
+    {
+        private readonly SQLiteAsyncConnection _db;
+
+        public DatabaseService(string dbPath)
+        {
+            _db = new SQLiteAsyncConnection(dbPath);
+            _db.CreateTableAsync<Producto>().Wait();
+        }
+
+        public Task<int> AddProductoAsync(Producto producto)
+        {
+            return _db.InsertAsync(producto);
+        }
+
+        public Task<List<Producto>> GetProductosAsync()
+        {
+            return _db.Table<Producto>().ToListAsync();
+        }
+    }
+}
+
+
+
+
+
