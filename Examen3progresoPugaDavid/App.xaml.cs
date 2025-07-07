f8e262882ae258ed95d374f3b56e5dbccf56091a
laboratorio <laboratorio@UPOAULA10724>
2025-07-07 07:11:02 -0500

    Agregar archivos de proyecto.

---
+++ Examen3progresoPugaDavid/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Examen3progresoPugaDavid
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Examen3progresoPugaDavid/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Examen3progresoPugaDavid;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
