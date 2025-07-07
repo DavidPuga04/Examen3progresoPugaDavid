4ae7d9ff7a4f11b4fecc1fb5358301db6ff0cdf0
Laboratorio <laboratorio@udla-ec.int>
2025-07-07 08:48:21 -0500

    Creacion de las vistas

---
+++ Examen3progresoPugaDavid/Views/ListaLogsPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace Examen3progresoPugaDavid.Views;
+
+public partial class ListaLogsPage : ContentPage
+{
+	public ListaLogsPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Examen3progresoPugaDavid/Views/ListaProductosPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace Examen3progresoPugaDavid.Views;
+
+public partial class ListaProductosPage : ContentPage
+{
+	public ListaProductosPage()
+	{
+		InitializeComponent();
+	}
+}
