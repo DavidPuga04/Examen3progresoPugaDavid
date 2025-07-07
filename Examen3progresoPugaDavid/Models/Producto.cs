6d313fbb6b514c4933b2f08c103ca244649c8376
Laboratorio <laboratorio@udla-ec.int>
2025-07-07 08:08:07 -0500

    COrreccion de la pagina agregar productos

--- Examen3progresoPugaDavid/Models/Producto.cs
+++ Examen3progresoPugaDavid/Models/Producto.cs
@@ -8,5 +8,13 @@ namespace Examen3progresoPugaDavid.Models
 {
     class Producto
     {
+        [SQLite.PrimaryKey, SQLite.AutoIncrement]
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Categoria { get; set; }
+        public int Cantidad { get; set; }
+        public decimal PrecioEstimado { get; set; }
     }
 }
+
+
