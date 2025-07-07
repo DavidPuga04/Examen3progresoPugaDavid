aeca93e76ffaced179c26884ed99999c0e79e3bc
Laboratorio <laboratorio@udla-ec.int>
2025-07-07 07:42:50 -0500

    Se creo para agregar el producto

---
+++ Examen3progresoPugaDavid/Services/LogService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen3progresoPugaDavid.Services
+{
+    class LogService
+    {
+        private static string logFilepath = Path.Combine(FileSystem.AppDataDirectory, "log_Puga.txt");
+    
+
+    public static async Task AppendLogAsync(string NombreProducto)
+        {
+            string log = $"Se incluyó el registro [{NombreProducto}] en la base de datos el {DateTime.Now: yyyy-MM-dd HH:mm:ss}\n";
+            await File.AppendAllLinesAsync(logFilepath, new[] { log });
+        }
+        public static async Task<List<string>> LeerLogsAsync()
+        {
+            if (File.Exists(logFilepath))
+                return (await File.ReadAllLinesAsync(logFilepath)).ToList();
+            else
+                return new List<string>();
+        }
+    }
+}
+
